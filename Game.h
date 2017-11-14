dda43d9393558d0bf306c662a6fd519118f06d36
Ørjan Monsen <mons@dal.ca>
2017-11-14 19:22:16 -0400

    Updated game.h
    updated code style and indentation

--- Game.h
+++ Game.h
@@ -5,59 +5,65 @@
 #include <unistd.h>
 #include <string.h>
 using namespace std;
-//CLASS GAME
+
 class Game{
-		protected: // VAROIABLES
-			int num_of_players;
-			int winnerId;
-			int winnerScore;
-			int* score;
-			int totalTurns=-1;
-			int turn=0;
-			// CONSTRUCTR PARAETER NUM = NUMBER OF PAYLES
-			Game(int num){
-				num_of_players=num;
-				score = new int[num]; // ALLOCATES MEMORY
-			}
-			~Game(){} // DECONSTRUCTOR
-			// UDATE TURN INCREASES THE TURN
-			void updateTurn(){
-				turn++;
-			}
+	protected: 
+		// variables
+		int num_of_players;
+		int winnerId;
+		int winnerScore;
+		int* score;
+		int totalTurns=-1;
+		int turn=0;
+	
+		// constructor with num_of_players input
+		Game(int num) {
+			num_of_players = num;
+			score = new int[num]; // ALLOCATES MEMORY
+		}
+		
+		// deconstructor
+		~Game(){}
+	
+		// function to increment the turn
+		void updateTurn(){
+			turn++;
+		}
 
-			// CHECK TURN RETURNSTURN
-			int checkTurn(){
-				return turn;
-			}
-			// SET ROUND UPDATES THE MAX NUMBER OF ROUNDS
-			void setRounds(int r){
-				totalTurns=r;
-			}
+		// getter for the turn
+		int checkTurn(){
+			return turn;
+		}
+	
+		// setter for totalTurns
+		void setRounds(int r){
+			totalTurns = r;
+		}
 
-			// GAME ON RETURNS ONE WHILE THERE ARE TURN REMAINING
-			int gameOn(){
-				if(turn>totalTurns){
-					return 0;
-				}
-				else{
-					return 1;
-				}
+		// returns 1 while there are turns remaining
+		int gameOn(){
+			if (turn > totalTurns) {
+				return 0;
+			} else {
+				return 1;
 			}
+		}
 
-			// UPDATE SCORE  and winner
-			void updateScore(int player, int points){
-				score[player] = points;
-				if(points>winnerScore){
-					winnerScore=points;
-					winnerId=player;
-				}
+		// updated the score and winner
+		void updateScore(int player, int points) {
+			score[player] = points;
+			
+			if (points > winnerScore) {
+				winnerScore = points;
+				winnerId = player;
 			}
-			// returns the winner and prints the points
-			int getWinner(){
-
-				cout<<"POINTS: "<< winnerScore<<endl;
-				return winnerId;
-			}
+		}
+	
+		// returns the winner and prints the points
+		int getWinner() {
+			cout << "POINTS: " << winnerScore << endl;
+			return winnerId;
+		}
 };
 
 #endif
