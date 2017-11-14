e0855ee05b4907467c2a25aa23901985d76bcab5
Ørjan Monsen <mons@dal.ca>
2017-11-14 19:31:09 -0400

    Updated yahtzee.h
    Fixed indentation

--- yahtzee.h
+++ yahtzee.h
@@ -13,15 +13,15 @@
 using namespace std;
 
 class yahtzee: Game {
-public:
-	void rollTheDice(Rules& r, ScoreBoard& s, Dice& d, int rollsLeft, int turn);
-	void selectUpdate(Rules& r, ScoreBoard& s, Dice& d, int player);
-	int keeper[5] = {-1, -1, -1, -1, -1};
-	char c[2];
-	int turns= 0;
-	yahtzee(int players);
-	~yahtzee();
-	void playYathzee();
+	public:
+		void rollTheDice(Rules& r, ScoreBoard& s, Dice& d, int rollsLeft, int turn);
+		void selectUpdate(Rules& r, ScoreBoard& s, Dice& d, int player);
+		int keeper[5] = {-1, -1, -1, -1, -1};
+		char c[2];
+		int turns= 0;
+		yahtzee(int players);
+		~yahtzee();
+		void playYathzee();
 };
 
 #endif
